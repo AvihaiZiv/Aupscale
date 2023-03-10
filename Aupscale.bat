a905a1d323c388c14537e1f632d2746a2a29b2c3
Avihai Ziv <avihai40@gmail.com>
2023-03-10 23:07:42 +0200

    minor script changes

--- Aupscale.bat
+++ Aupscale.bat
@@ -2,6 +2,8 @@
 type Dev.md
 SET script_path=%~dp0
 SET waifu2x_path_exe=%script_path%waifu2x\waifu2x-ncnn-vulkan.exe
+SET ffmpeg_path_exe=%script_path%ffmpeg\ffmpeg.exe
+SET ffprobe_path_exe=%script_path%ffmpeg\ffprobe.exe
 SET frames_path=%script_path%Frames\
 SET upscaled_path=%script_path%Upscaled\
 set Export_path=%script_path%Export\
@@ -27,18 +29,18 @@ for %%F in ("%file_path%") do echo %%~nxF > file_pathtmp
 set /p file_name= <file_pathtmp
 del file_pathtmp
 echo Getting the frame rate of the video
-ffprobe.exe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate "%file_path%" > fpout
+%ffprobe_path_exe% -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate "%file_path%" > fpout
 set /p frame_rate= <fpout
 del fpout
 echo Encodiing audio from original video to Opus
 mkdir "%file_name%-audio"
-ffmpeg.exe -i "%file_path%" -c:a libopus -vbr on -b:a 192k "%file_name%-audio\audio.opus"
+%ffmpeg_path_exe% -i "%file_path%" -c:a libopus -vbr on -b:a 192k "%file_name%-audio\audio.opus"
 echo Extracting frames from video
-ffmpeg.exe -i "%file_path%" -r %frame_rate% %frames_path%frame%%09d.png
+%ffmpeg_path_exe% -i "%file_path%" -r %frame_rate% %frames_path%frame%%09d.png
 echo Upscaling frames
 %waifu2x_path_exe% -i %frames_path% -o %upscaled_path% -s %scale% -n %noise% -v
 echo Rendering upscaled video
-ffmpeg.exe -framerate %frame_rate% -pattern_type sequence -start_number 000000001 -i %upscaled_path%frame%%09d.png -i "%file_name%-audio\audio.opus" -c:a copy -shortest -c:v libx265 -crf 19 -preset slow -x265-params "limit-sao=1:bframes=8:psy-rd=1:aq-mode=3" "%Export_path%%file_name%.upscaled.mkv"
+%ffmpeg_path_exe% -framerate %frame_rate% -pattern_type sequence -start_number 000000001 -i %upscaled_path%frame%%09d.png -i "%file_name%-audio\audio.opus" -c:a copy -shortest -c:v libx265 -crf 19 -preset slow -x265-params "limit-sao=1:bframes=8:psy-rd=1:aq-mode=3" "%Export_path%%file_name%.upscaled.mkv"
 rmdir /s /q "%file_name%"-audio
 del /s /q %frames_path%*
 del /s /q %upscaled_path%*
